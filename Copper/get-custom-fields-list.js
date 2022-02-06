eea420328105f32e7b17a548652631773a05ecc1
Infinity Web <52179747+infinity-web-admin@users.noreply.github.com>
2022-02-06 12:21:42 -0500

    Update get-custom-fields-list.js

--- Copper/get-custom-fields-list.js
+++ Copper/get-custom-fields-list.js
@@ -1,10 +1,10 @@
 // Retrieve List of Custom Fields
-async function getCustomFieldsList(coKey, keyOwner){
+async function getCustomFieldsList(coKey, coEmail){
     console.log('Retrieving Custom Fields list from Copper...');
     let apiUrl= "https://api.prosperworks.com/developer_api/v1/custom_field_definitions";
-    let data = await fetch(apiUrl, {method: "GET", headers: {'X-PW-AccessToken': coKey, 'X-PW-Application': 'developer_api', 'X-PW-UserEmail': keyOwner, 'Content-Type': 'application/json'}});
+    let data = await fetch(apiUrl, {method: "GET", headers: {'X-PW-AccessToken': coKey, 'X-PW-Application': 'developer_api', 'X-PW-UserEmail': coEmail, 'Content-Type': 'application/json'}});
     //console.log(data);
     let apiData = await data.json();
     console.log(apiData);
-    return apiData; 
-};
+    return apiData
+}
